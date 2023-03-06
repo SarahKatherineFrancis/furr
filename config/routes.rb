6651d3c09cbf8e0d6d7f6cb937fe5b4b698b4c98
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-03-06 17:50:41 +0200

    added user only show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
 
   # Defines the root path route ("/")
   resources :petsitters, only: [:index, :show] do
