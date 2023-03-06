2df0862acc129d15394c302c1d34510f86b77537
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-03-06 18:02:01 +0200

    added validations to the models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  belongs_to :petsitters
+  belongs_to :petsitter
+
+  validates :start_date, :end_date, :service, :pet_name, :pet_type, presence: true
 end
