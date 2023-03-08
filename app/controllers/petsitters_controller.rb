2528b7aee9504d166812cb62642de77ab0169a78
Oscar Welton <oscarwelton@gmail.com>
2023-03-08 19:56:40 +0200

    made a responsive star rating scale

--- app/controllers/petsitters_controller.rb
+++ app/controllers/petsitters_controller.rb
@@ -7,8 +7,8 @@ class PetsittersController < ApplicationController
     @petsitter = Petsitter.find(params[:id])
     @booking = Booking.new
     @reviews = Review.all
-
-  end
+    @sitter_reviews = sitter_reviews_counter
+    end
 
   def new
     @petsitter = Petsitter.new
@@ -18,6 +18,17 @@ class PetsittersController < ApplicationController
     @petsitter = Petsitter.new(petsitter_params)
     @petsitter.user_id = current_user.id
     redirect_to root_path if @petsitter.save
+  end
+
+  def sitter_reviews_counter
+    sitter_reviews = []
+    @reviews = Review.all
+    @reviews.each do |review|
+      if review.booking.petsitter.id == @petsitter.id
+        sitter_reviews << review
+      end
+    return sitter_reviews.count
+    end
   end
 
   private
