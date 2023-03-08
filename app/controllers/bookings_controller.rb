31918c1a0ad0a0f99d2b5dd35a6dbe5ed559a581
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-03-08 16:06:39 +0200

    nono

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,4 +4,19 @@ class BookingsController < ApplicationController
   def my_bookings
     @bookings = current_user.bookings
   end
+
+  def create
+    @petsitter = Petsitter.find(params[:petsitter_id])
+    @booking = @petsitter.bookings.build(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to my_bookings_path
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :service, :pet_name, :pet_type)
+  end
 end
