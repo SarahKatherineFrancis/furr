69f082d2b350ca1be6287d4c802fe40390f64b03
Kirsty Brown <kirstyannbrown4@gmail.com>
2023-03-09 19:14:17 +0200

    markers and info working for users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,9 @@ class UsersController < ApplicationController
     @markers = @users.geocoded.map do |user|
       {
         lat: user.latitude,
-        lng: user.longitude
+        lng: user.longitude,
+        info_window_html: render_to_string(partial: "/shared/info_window", locals: {user: user}),
+        marker_html: render_to_string(partial: "/shared/marker")
       }
     end
   end
