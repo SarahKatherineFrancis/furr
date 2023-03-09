44ed4bdcf163f66cdf27cb4e764c2e44047aaa89
Oscar Welton <oscarwelton@gmail.com>
2023-03-09 12:52:20 +0200

    editing the reviews still

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,6 +8,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.booking_id = params[:booking_id]
     redirect_to root_path if @review.save
+    raise
   end
 
   private
