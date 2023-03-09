f301eb39a5b318708eb8eb23c6306b8d9bb9564e
Oscar Welton <oscarwelton@gmail.com>
2023-03-09 12:31:13 +0200

    added the review option to a booking.

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/my_bookings', to: 'bookings#my_bookings', as: 'my_bookings'
 
-  resources :bookings, only: [:create, :index]
+  resources :bookings, only: [:create, :index] do
+    resources :reviews, only: [:new]
+  end
+
+  resources :reviews
 
   resources :users, only: [:show]
 
-
   get '/my_profile', to: 'users#show', id: 'current'
-  # Defines the root path route ("/")
   resources :petsitters, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:create, :show, :edit, :update, :destroy] do
-      resources :reviews, only: [:show, :index, :new, :create]
-    end
+    resources :bookings, only: [:create, :show, :edit, :update, :destroy]
   end
 end
