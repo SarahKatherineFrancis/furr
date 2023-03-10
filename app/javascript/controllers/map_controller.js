4009c997829e70899ea71762d95542335da8dc42
Kirsty Brown <kirstyannbrown4@gmail.com>
2023-03-10 16:27:57 +0200

    fixed final

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -32,17 +32,11 @@ export default class extends Controller {
         .setPopup(popup)
         .addTo(this.map)
     });
+
+    this.map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+    }))
+
   }
-this.#addMarkersToMap()
-this.#fitMapToMarkers()
-
-this.map.addControl(new MapboxGeocoder({
-  accessToken: mapboxgl.accessToken,
-  mapboxgl: mapboxgl
-}))
-
 }
-
-this.map = new mapboxgl.Map({
-  container: this.element,
-  style: "mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb"})
