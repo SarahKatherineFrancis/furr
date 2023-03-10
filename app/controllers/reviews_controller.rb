99bc73c55748d840de6733f1b4ce5d933724b514
Oscar Welton <oscarwelton@gmail.com>
2023-03-10 15:29:04 +0200

    reviews are back baby

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,19 +2,19 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    @booking = Booking.find(params[:booking_id])
   end
 
   def create
     @review = Review.new(review_params)
-    @review.booking_id = params[:booking_id]
-    raise
-    redirect_to root_path if @review.save
-
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
+    redirect_to petsitter_path(@review.booking.petsitter) if @review.save
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:comment, :raiting)
+    params.require(:review).permit(:comment, :rating)
   end
 end
