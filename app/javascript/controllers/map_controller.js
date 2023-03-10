2c8857d03bc92fe83aa6aa8d7ee9be024624e4a9
Kirsty Brown <kirstyannbrown4@gmail.com>
2023-03-10 14:41:36 +0200

    some frontend

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -16,27 +16,33 @@ export default class extends Controller {
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
-}
+  }
 
-#fitMapToMarkers() {
-  const bounds = new mapboxgl.LngLatBounds()
-  this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-  this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-}
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
 
-#addMarkersToMap() {
-  this.markersValue.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup)
-      .addTo(this.map)
-  });
-}
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(this.map)
+    });
+  }
 this.#addMarkersToMap()
-    this.#fitMapToMarkers()
+this.#fitMapToMarkers()
 
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-                                        mapboxgl: mapboxgl }))
+this.map.addControl(new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken,
+  mapboxgl: mapboxgl
+}))
 
 }
+
+this.map = new mapboxgl.Map({
+  container: this.element,
+  style: "mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb"})
